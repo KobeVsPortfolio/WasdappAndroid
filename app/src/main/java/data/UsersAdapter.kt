3540613c05f4207e911efdcf3a903cf3d05f180c
joren <joren.vanherwegen@hotmail.com>
2019-05-27 16:59:08 +0200

    delete user fixed

--- app/src/main/java/data/UsersAdapter.kt
+++ app/src/main/java/data/UsersAdapter.kt
@@ -1,25 +1,30 @@
 
 package data
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.support.v7.widget.RecyclerView
-import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
+import com.example.wasdappapp.ListUsersActivity
 import com.example.wasdappapp.R
-import com.example.wasdappapp.ThisObjectActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import model.User
-import model.WasdappEntry
-import java.lang.Exception
+
 
 class UsersAdapter(private val list: ArrayList<User>, private val context: Context) :
     RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
+
+    val db = FirebaseFirestore.getInstance()
+    val collection = db.collection("users")
+
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItem(u: User) {
@@ -30,9 +35,29 @@ class UsersAdapter(private val list: ArrayList<User>, private val context: Conte
             val objectIsAdmin: TextView = itemView.findViewById(R.id.function_of_user) as TextView
             objectIsAdmin.text = u.role
 
-            /*itemView.findViewById(R.id.bin_button).setOnClickListener {
+            var bin_button : Button = itemView.findViewById(R.id.bin_button_user) as Button
+            bin_button.setOnClickListener {
+                val deleteAlert = AlertDialog.Builder(context)
+                deleteAlert.setTitle("Delete object")
+                deleteAlert.setMessage(
+                    "Are you sure you want to delete this user?"
+                )
+                deleteAlert.setPositiveButton("Delete") { dialogInterface: DialogInterface, i: Int ->
+                    collection.document("${u.email}").delete()
+                    val intent = Intent(context, ListUsersActivity::class.java)
+                    context.startActivity(intent)            }
 
-            }*/
+                deleteAlert.setNegativeButton("Cancel") { dialogInterface: DialogInterface, i: Int ->
+                }
+
+                val alertDialog: AlertDialog = deleteAlert.create()
+                alertDialog.show()
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(context.getColor(R.color.colorWarning))
+                val textView = alertDialog.findViewById<TextView>(android.R.id.message)
+                textView?.setTextColor(context.getColor(R.color.colorAccent))
+                alertDialog.window?.setBackgroundDrawableResource(R.color.colorPrimary)
+
+            }
         }
 
     }
