b674722fe326d420909116244f3c07548a9d7f79
JasperVB3 <jasper.vandenbossche@hotmail.com>
2019-05-28 11:31:22 +0200

    edit and delete users

--- app/src/main/java/data/UsersAdapter.kt
+++ app/src/main/java/data/UsersAdapter.kt
@@ -1,17 +1,22 @@
-
 package data
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.example.wasdappapp.ListActivity
 import com.example.wasdappapp.R
 import com.example.wasdappapp.ThisObjectActivity
 import model.User
@@ -23,6 +28,7 @@ class UsersAdapter(private val list: ArrayList<User>, private val context: Conte
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItem(u: User) {
+            var editUser: Button = itemView.findViewById(R.id.edit_user) as Button
             val objectName: TextView = itemView.findViewById(R.id.name_of_user) as TextView
             objectName.text = u.email
             println("writing in adapter")
@@ -33,9 +39,52 @@ class UsersAdapter(private val list: ArrayList<User>, private val context: Conte
             /*itemView.findViewById(R.id.bin_button).setOnClickListener {
 
             }*/
+            editUser.setOnClickListener {
+                showDialog()
+            }
         }
 
     }
+
+    private fun showDialog() {
+        lateinit var dialog: AlertDialog
+        val array = arrayOf("Admin", "User")
+        val builder = AlertDialog.Builder(context)
+        var checkedItem : Int = -1
+        builder.setTitle("What do you want this user to be?")
+        builder.setSingleChoiceItems(array, checkedItem) { _, which ->
+
+                try {
+                    Toast.makeText(
+                        context,
+                        "changed succesfully",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                } catch (e: IllegalArgumentException) {
+                    Toast.makeText(
+                        context,
+                        "Something went wrong. Please try again.",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
+            builder.setPositiveButton("confirm") { dialogInterface: DialogInterface, i: Int ->
+            }
+            builder.setNegativeButton("cancel") { dialogInterface: DialogInterface, i: Int ->
+            }
+
+
+
+
+        // Initialize the AlertDialog using builder object
+        dialog = builder.create()
+
+        // Finally, display the alert dialog
+        dialog.show()
+    }
+
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): UsersAdapter.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.activity_admin_users, p0, false)
