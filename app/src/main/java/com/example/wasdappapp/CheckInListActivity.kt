9dcb973da5ebb2b6f4af7b5a62fdd471b8c928b2
KevinDeClerck <kevin.declerck96@gmail.com>
2019-05-29 13:04:13 +0200

    Checkin pagination

--- app/src/main/java/com/example/wasdappapp/CheckInListActivity.kt
+++ app/src/main/java/com/example/wasdappapp/CheckInListActivity.kt
@@ -1,15 +1,19 @@
 package com.example.wasdappapp
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import data.CheckInAdapter
 import kotlinx.android.synthetic.main.activity_check_in_list.*
+import kotlinx.android.synthetic.main.activity_list.*
 import model.CheckIn
 import model.WasdappEntry
 import java.util.*
@@ -22,6 +26,8 @@ class CheckInListActivity : AppCompatActivity() {
     val auth = FirebaseAuth.getInstance()
     val db = FirebaseFirestore.getInstance()
     private val currentUser = auth.currentUser
+    var next: Query? = null
+    var lastVisible: DocumentSnapshot? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +36,22 @@ class CheckInListActivity : AppCompatActivity() {
         val wasdappobj = intent.getParcelableExtra("wasdappobj") as WasdappEntry
 
         val checkInList: ArrayList<CheckIn> = ArrayList()
-        db.collection("wasdapps/${wasdappobj.id}/checkins").get().addOnSuccessListener { task ->
-            for(i in task.documents){
+
+        val first = db.collection("wasdapps/${wasdappobj.id}/checkins").limit(8)
+
+        first.get()
+            .addOnSuccessListener { task ->
+                if(!task.isEmpty){
+                lastVisible = task.documents[task.size() - 1]
+                next = db.collection("wasdapps/${wasdappobj.id}/checkins")
+                    .startAfter(lastVisible!!)
+                    .limit(5)
+
+            for(i in task.documents) {
                 val checkIn = i.toObject(CheckIn::class.java)
                 checkInList.add(checkIn!!)
+
+            }
                 layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
                 adapter = CheckInAdapter(checkInList, this)
 
@@ -42,6 +60,44 @@ class CheckInListActivity : AppCompatActivity() {
                 adapter!!.notifyDataSetChanged()
             }
         }
+
+        btn_checkin_more.setOnClickListener{
+            next!!.get()
+                .addOnSuccessListener { task ->
+                    if(!task.isEmpty){
+                        //if (task.documents[task.size() - 1].exists()) {
+                        lastVisible = task.documents[task.size() - 1]
+                        next = db.collection("wasdapps/${wasdappobj.id}/checkins")
+                            .startAfter(lastVisible!!)
+                            .limit(5)
+                        for ( i in task.documents!!) {
+                            val checkIn = i.toObject(CheckIn::class.java)
+                            checkInList.add(checkIn!!)
+                        }
+                    } else if(task.isEmpty){
+                        btn_checkin_more.text = "Go to top"
+                    }
+                    rcv_checkins.layoutManager = layoutManager
+                    rcv_checkins.adapter = adapter
+                    adapter!!.notifyDataSetChanged()
+                }
+        }
+
+        btn_checkin_more.visibility = View.GONE
+        rcv_checkins.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if(!rcv_checkins.canScrollVertically(1)){
+                    println("Last item.... ")
+                    btn_checkin_more.visibility = View.VISIBLE
+                }else{
+                    btn_checkin_more.visibility = View.GONE
+                }
+
+            }
+        })
 
         btn_checkin.setOnClickListener {
             val addedCheckin = CheckIn()
